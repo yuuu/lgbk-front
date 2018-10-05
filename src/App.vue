f1ef5618e863305da3a80e44c6a5591283fb339d
Yuhei Okazaki <okazaki@fusic.co.jp>
2018-10-05 13:16:46 +0900

    コンポーネントに分割

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,21 @@
 <template>
   <div id="app">
-    <nav-bar />
+    <header>
+      <nav-bar />
+    </header>
     <main>
-      <b-container>
-        <b-row class="justify-content-center">
-          <b-col md="6">
-            <b-card>
-              <h3 slot="header">Register</h3>
-              <b-card-body>
-                <b-form @submit="onSubmit" @reset="onReset">
-                  <b-form-group id="email-address" label="Email address" label-for="email-address-input" description="Enter email">
-                    <b-form-input id="email-address-input" type="email" v-model="email" required placeholder="Enter email" />
-                  </b-form-group>
-                  <b-form-group id="password" label="Password" label-for="password-input" description="Enter password">
-                    <b-form-input id="password-input" type="password" v-model="password" required placeholder="Enter password" />
-                  </b-form-group>
-                  <b-form-group id="remember">
-                    <b-form-checkbox v-model="remember">Remember me</b-form-checkbox>
-                  </b-form-group>
-                  <b-button type="submit" variant="primary">Submit</b-button>
-                  <b-button type="reset" variant="danger">Reset</b-button>
-                  <a href="#" class="btn btn-link">Forgot Your Password?</a>
-                </b-form>
-              </b-card-body>
-            </b-card>
-          </b-col>
-        </b-row>
-      </b-container>
+      <register />
     </main>
   </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue'
+import Register from './components/Register.vue'
 
 export default {
   name: 'app',
-  components: { NavBar },
-  data() {
-    return {
-      email: '',
-      password: '',
-      remember: false
-    }
-  },
-  methods: {
-    onSubmit: function () {
-      // TODO
-    },
-    onReset: function () {
-      this.email = '';
-      this.password = '';
-      this.remember = false;
-    }
-  }
+  components: { NavBar, Register },
 }
 </script>
 
