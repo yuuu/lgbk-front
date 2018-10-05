b91f220e08a44e77c8dda8f6375b81d4c3ddd6e8
Yuhei Okazaki <okazaki@fusic.co.jp>
2018-10-05 12:28:34 +0900

    ログインページのデザイン

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav-bar />
+    <main>
+      <b-container>
+        <b-row class="justify-content-center">
+          <b-col md="6">
+            <b-card>
+              <h3 slot="header">Register</h3>
+              <b-card-body>
+                <b-form @submit="onSubmit" @reset="onReset">
+                  <b-form-group id="email-address" label="Email address" label-for="email-address-input" description="Enter email">
+                    <b-form-input id="email-address-input" type="email" v-model="email" required placeholder="Enter email" />
+                  </b-form-group>
+                  <b-form-group id="password" label="Password" label-for="password-input" description="Enter password">
+                    <b-form-input id="password-input" type="password" v-model="password" required placeholder="Enter password" />
+                  </b-form-group>
+                  <b-form-group id="remember">
+                    <b-form-checkbox v-model="remember">Remember me</b-form-checkbox>
+                  </b-form-group>
+                  <b-button type="submit" variant="primary">Submit</b-button>
+                  <b-button type="reset" variant="danger">Reset</b-button>
+                  <a href="#" class="btn btn-link">Forgot Your Password?</a>
+                </b-form>
+              </b-card-body>
+            </b-card>
+          </b-col>
+        </b-row>
+      </b-container>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  components: { NavBar },
+  data() {
+    return {
+      email: '',
+      password: '',
+      remember: false
+    }
+  },
+  methods: {
+    onSubmit: function () {
+      // TODO
+    },
+    onReset: function () {
+      this.email = '';
+      this.password = '';
+      this.remember = false;
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped lang="scss">
+  main {
+    padding: 40px 20px;
+  }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-navbar toggleable="md" type="dark" variant="primary">
+    <b-navbar-brand href="#">Lgbk</b-navbar-brand>
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <b-collapse is-nav id="nav_collapse">
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#">Login</b-nav-item>
+        <b-nav-item href="#">Register</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style scoped lang="scss">
+</style>
